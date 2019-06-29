c135d9dbd88960afd9afd6de9bd4b3adb831da21
ahmed GHOUL <ghoul.ahmed5@gmail.com>
2019-06-29 10:48:45 +0100

    example with map operation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+    const http$ = this.createHttpObserver();
+
+    const course$ = http$.pipe(
+      map(res => res['payload'])
+    );
+    course$.subscribe(
+      courses => console.log(courses),
+      () => { },
+      () => console.log('complete'))
+  }
+  /**
+   *
+   *
+   * @memberof AppComponent
+   */
+  createHttpObserver() {
+    return Observable.create(observer => {
+      fetch('/api/courses').then(
+        res => res.json()
+      ).then(body => {
+        observer.next(body);
+        observer.complet();
+      }).catch(err => {
+        observer.error(err);
+      });
+
+    });
+
+  }
 }
