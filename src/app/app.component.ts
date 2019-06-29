692fbc938de3fa474bf237c1079885e6aa8943be
ahmed GHOUL <ghoul.ahmed5@gmail.com>
2019-06-29 10:05:18 +0100

    create simple observable with fetch promise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+    const http$ = Observable.create(observer => {
+      fetch('/api/courses').then(
+        res => res.json()
+      ).then(body => {
+        observer.next(body);
+        observer.complet();
+      }).catch(err => {
+        observer.error(err);
+      });
+
+    });
+
+  http$.subscribe(courses=> console.log(courses), ()=>{}, ()=>console.log('complete'))
+  }
 }
